67fdb557c642d9ab2e4c2c94a9f9d596caffd2a6
JotazizuSensei <jota.zizu@gmail.com>
2026-08-20 14:28:08 +0100

    Add offline service worker for BLUE Symbiote v10

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+const CACHE='blue-symbiote-v10-20260820';
+const CORE=['./','./index.html','./manifest.json','./styles.css','./data-p1.js','./data-p2.js','./data-p3.js','./code-p1.js','./code-p2.js','./code-p3.js','./boot.js','./icon-180.png','./icon-192.png','./icon-512.png','./icon-512-maskable.png'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting()});
+self.addEventListener('fetch',event=>{const req=event.request;if(req.method!=='GET')return;const url=new URL(req.url);if(url.origin!==self.location.origin)return;if(req.mode==='navigate'){event.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return res}).catch(()=>caches.match('./index.html')));return}event.respondWith(caches.match(req).then(hit=>hit||fetch(req).then(res=>{if(res&&res.status===200){const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy))}return res}).catch(()=>caches.match('./index.html'))))});
