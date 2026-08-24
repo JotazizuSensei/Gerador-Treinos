d43c32b4e8756eac6e68cfa441f3a7b4e951444c
JotazizuSensei <jota.zizu@gmail.com>
2026-08-24 13:19:46 +0100

    BLUE v10.0.10: stable service worker without forced reload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const VERSION='10.0.9';
-const CACHE='blue-symbiote-auto-'+VERSION;
-const CORE=['./','./index.html','./manifest.json','./update-10.0.9.html','./patch-10.0.3.js','./patch-10.0.4.js','./patch-10.0.5.js','./patch-10.0.6.js','./patch-10.0.7.js','./patch-10.0.8.js','./payload-1a.txt','./payload-1b.txt','./payload-1c.txt','./payload-1d.txt','./payload-2a.txt','./payload-2b.txt','./payload-2c.txt','./payload-2d.txt','./payload-3.txt','./payload-4.txt','./icon-180.png','./icon-192.png','./icon-512.png','./icon-512-maskable.png'];
+const VERSION='10.0.10';
+const CACHE='blue-symbiote-stable-'+VERSION;
+const CORE=['./','./index.html','./stable-10.0.10.html','./manifest.json','./patch-10.0.3.js','./patch-10.0.4.js','./patch-10.0.5.js','./patch-10.0.6.js','./patch-10.0.7.js','./patch-10.0.8.js','./patch-10.0.10.js','./payload-1a.txt','./payload-1b.txt','./payload-1c.txt','./payload-1d.txt','./payload-2a.txt','./payload-2b.txt','./payload-2c.txt','./payload-2d.txt','./payload-3.txt','./payload-4.txt','./icon-180.png','./icon-192.png','./icon-512.png','./icon-512-maskable.png'];
 self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
 });
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('blue-symbiote-')&&k!==CACHE).map(k=>caches.delete(k)))));
 });
-self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting();});
+self.addEventListener('message',()=>{});
 self.addEventListener('fetch',event=>{
   const req=event.request;if(req.method!=='GET')return;
   const url=new URL(req.url);if(url.origin!==self.location.origin)return;
